380666954de51dfe2a55f0fbe6a184da8fa0be59
JonathanMontoya <montoya0396@gmail.com>
2025-12-29 14:46:08 -0500

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,27 @@
 // const express = require('express') // default is this which is type of commonjs
 import express from 'express' // changed type to module in package.json to allow this to work
 import dotenv from 'dotenv'
+import path from 'path'
 
 import authRoutes from './routes/auth.route.js'
 import messagesRoutes from './routes/message.route.js'
 
 dotenv.config()
 const app = express()
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000
 
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messagesRoutes)
+
+// Make ready for deployment
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"))
+})
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on port http://localhost:${PORT}/`)
