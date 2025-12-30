1976b105fc1e471fc1509795bb973932fc377fb4
Jonathan Montoya <montoya0396@gmail.com>
2025-12-30 15:09:21 -0500

    implemented welcome email feature

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 // const express = require('express') // default is this which is type of commonjs
 import express from 'express' // changed type to module in package.json to allow this to work
-import dotenv from 'dotenv'
+import { ENV } from './lib/env.js'
 import path from 'path'
 
 import {connectDB} from './lib/db.js'
@@ -9,10 +9,9 @@ import authRoutes from './routes/auth.route.js'
 import messagesRoutes from './routes/message.route.js'
 import { connect } from 'http2'
 
-dotenv.config()
 const app = express()
 const __dirname = path.resolve();
-const PORT = process.env.PORT || 3000
+const PORT = ENV.PORT || 3000
 
 app.use(express.json()) // req.body
 
@@ -20,7 +19,7 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messagesRoutes)
 
 // Make ready for deployment
-if(process.env.NODE_ENV === "production") {
+if(ENV.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/dist")))
 
 app.get("*", (req, res) => {
